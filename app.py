564f9d49ad4cf7397f924dc373e25a67dbf88e09
NikolaTareyn <niktareyn@gmail.com>
2026-03-27 17:30:50 +0200

    fix: remove duplicate database

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app) 
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'wazzer.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'instance/wazzer.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
